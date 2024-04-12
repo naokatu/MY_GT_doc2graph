4c2e135283339dc1622b22f3610ea976228a2250
naokatu <naokatu4@gmail.com>
2024-04-12 10:51:42 +0900

    fix: セグフォ

--- src/phrase_extraction_by_spacy.py
+++ src/phrase_extraction_by_spacy.py
@@ -1,16 +1,13 @@
 import spacy
-from typing import List, Tuple
-import make_corpus_from_dataset
+from typing import List
 
 def noun_phrases_extraction_by_spacy(doc) -> List[str]:
 
     # 名詞句の抽出　あまりに短い文章だと精度が落ちるかも
     noun_list = []
     for chunk in doc.noun_chunks:
-        print(chunk.text)
         noun_list.append(chunk.text)
 
-    print(noun_list)
     return noun_list
 
 def verb_phrases_extract_by_spacy(doc) -> List[str]:
@@ -34,7 +31,6 @@ def verb_phrases_extract_by_spacy(doc) -> List[str]:
 
             verb_phrases.append(''.join(verb_phrase))
             
-    print(verb_phrases)
     return verb_phrases
 
 def adj_phrases_extraction_by_spacy(doc) -> List[str]:
@@ -45,11 +41,14 @@ def adj_phrases_extraction_by_spacy(doc) -> List[str]:
         next_token = token.doc[token.i+1] if not token.is_sent_end else None
         prev_token = token.doc[token.i-1] if not token.is_sent_start else None
         if next_token is not None:
+            is_token_tag = False
+            is_next_token_tag = False
             # 品詞を抽出
             token_tag = token.tag_.split('-')[0]
-            if len(token.tag_.split('-')) >= 2 and len(token.tag_.split('-')) >= 2: 
+            if len(token.tag_.split('-')) >= 2 and len(next_token.tag_.split('-')) >= 2:
                 token_tag_sub = token.tag_.split('-')[1]
                 next_token_tag_sub = next_token.tag_.split('-')[1]
+            
             # 形容動詞 （形状詞 + な） ... 静かな
             if (token_tag == '形状詞') and (next_token.text == 'な'):
                 adj_phrases.append(token.text + 'な')
@@ -57,7 +56,7 @@ def adj_phrases_extraction_by_spacy(doc) -> List[str]:
             if (token_tag == '名詞') and (next_token.text == 'な'):
                 adj_phrases.append(token.text + 'な')
             # 動詞 + 形容詞的 ... わかりやすい, 怒りっぽい
-            if (token_tag == '動詞') and (next_token_tag_sub == '形容詞的'):
+            if is_next_token_tag and (token_tag == '動詞') and (next_token_tag_sub == '形容詞的'):
                 adj_phrases.append(token.text + next_token.text)
             # 単体の形容詞
             if token_tag == '形容詞':
@@ -66,30 +65,21 @@ def adj_phrases_extraction_by_spacy(doc) -> List[str]:
         if prev_token is not None:
             prev_token_tag = prev_token.tag_.split('-')[0]
             # 名詞 + ない ... 問題ない, 仕方ない
-            if (prev_token_tag == '名詞') and (token_tag == '形容詞') and (token_tag_sub == '非自立可能'):
+            if is_token_tag and (prev_token_tag == '名詞') and (token_tag == '形容詞') and (token_tag_sub == '非自立可能'):
                 adj_phrases.append(prev_token.text + token.text)
     
-    print(adj_phrases)
     return adj_phrases
 
 
 def main():
     nlp = spacy.load('ja_ginza')
-    # 100万文字以上は以下の設定をする必要あり
-    nlp.max_length = 1500000
-    text = make_corpus_from_dataset.return_livedoor_text('https://www.rondhuit.com/download/ldcc-20140209.tar.gz',
-                                                        'ldcc-20140209.tar.gz')
-    input_text = '彼女は毎朝熱いコーヒーを飲む'
-    doc = nlp(text)
-    doc2 = nlp(input_text)
+    input_text = '少年が真面目にサッカーをする'
+    
+    doc = nlp(input_text)
 
-    noun_phrases_extraction_by_spacy(doc2)
-    adj_phrases_extraction_by_spacy(doc2)
-    verb_phrases_extract_by_spacy(doc2)
-
-    noun_phrases_extraction_by_spacy(doc)
-    adj_phrases_extraction_by_spacy(doc)
-    verb_phrases_extract_by_spacy(doc)
+    print(noun_phrases_extraction_by_spacy(doc))
+    print(verb_phrases_extract_by_spacy(doc))
+    print(adj_phrases_extraction_by_spacy(doc))
 
 if __name__ == "__main__":
     main()
