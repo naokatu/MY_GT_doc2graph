29d5ee07dddc50e1712e6e1953627a31c166d47d
naokatu <naokatu4@gmail.com>
2024-04-12 10:52:44 +0900

    chore: read livedoor

--- src/read_graph_made_by_NetWorkX.py
+++ src/read_graph_made_by_NetWorkX.py
@@ -1,20 +1,23 @@
 import pickle
 import networkx as nx
 import matplotlib.pyplot as plt
+import gzip
+import japanize_matplotlib
 
 # G = nx.Graph()
 
-with open('../data/dblp.win5.pickle.gz', 'rb')as f:
+with open('../data/livedoor.win5.pickle.gz', 'rb')as f:
   all_graphs = pickle.load(f)
+  print(type(all_graphs))
   # グラフ数の取得
   lens = len(all_graphs)
 
-G = all_graphs[0]  # 最初のグラフを取得
+G = all_graphs[5]  # 最初のグラフを取得
 print(nx.number_of_nodes(G))
 print(nx.number_of_edges(G))
 # グラフの描画
 pos = nx.spring_layout(G)
-nx.draw_networkx(G, pos, with_labels=True, alpha=0.5)
+nx.draw_networkx(G, pos, with_labels=True, alpha=0.5, font_family='IPAexGothic')
 
 # 表示
 plt.axis("off")
